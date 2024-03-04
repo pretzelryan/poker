a40565d2f15f43e41475d923b65c79b28453a3c9
pretzelryan <muetzelryan@gmail.com>
2024-03-04 13:58:03 -0600

    Added basic functionality to Pot class.

--- poker/pot.py
+++ poker/pot.py
@@ -5,6 +5,10 @@
 # Author - Ryan Muetzel (@pretzelryan)
 #
 
+# Package imports.
+from .player import Player
+
+
 class Pot:
     """
     A class to track the money and players in each pot/side pot.
@@ -12,9 +16,33 @@ class Pot:
     """
 
     def __init__(self):
-        # TODO: Docs
-        self.chip_total = 0
+        """
+        Constructor.
+
+        """
+        self.chip_total = float(0)
         self.player_list = []
+        self.betting_active = True
 
     def __repr__(self):
-        return "Pot size: " + str(self.chip_total)
+        return "Pot size: " + str(self.get_chip_total())
+
+    def get_chip_total(self) -> float:
+        """
+        Accessor for the total chips in the pot.
+
+        :return: Float for the total number of chips.
+        """
+        return self.chip_total
+
+    def add_player(self, player: Player):
+        """
+        Adds an eligible player into the pot.
+
+        :param player: Player object who is active in the pot.
+        :return: None
+        """
+        self.player_list.append(player)
+
+    # TODO: Add money to pot
+    # TODO: Evaluate pot winner (Maybe do outside of this class)
