bd8c6eb5cc182cc2f1490a115063904210ba4d1a
pretzelryan <muetzelryan@gmail.com>
2024-03-08 16:17:32 -0600

    Added add and remove player functions to poker_game

--- poker/poker_game.py
+++ poker/poker_game.py
@@ -19,7 +19,28 @@ class PokerGame:
     """
 
     def __init__(self):
-        pass
+        self.player_list = []
+        self.pot_list = [Pot()]
+        self.deck = Deck()
+
+    def add_player(self, player: Player):
+        """
+        Adds specified player to the game.
+
+        :param player: Player object to join the game
+        :return: None
+        """
+        self.player_list.append(player)
+
+    def remove_player(self, player: Player):
+        """
+        Removes specified player from the game.
+
+        :param player: Player object to be removed.
+        :return: None
+        """
+        if player in self.player_list:
+            self.player_list.remove(player)
 
     def deal_players(self):
         pass
