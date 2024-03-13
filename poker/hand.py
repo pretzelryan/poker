ccc0169dae7131c9aa67e000e91cc79933c64e27
pretzelryan <muetzelryan@gmail.com>
2024-03-13 16:33:06 -0500

    Added skeleton methods for Hand Class.

--- poker/hand.py
+++ poker/hand.py
@@ -5,8 +5,7 @@
 # Author - Ryan Muetzel (@pretzelryan)
 #
 
-from enum import Enum
-from card import Card
+from .card import *
 
 
 class HandType(Enum):
@@ -33,20 +32,74 @@ class Hand:
 
     """
     def __init__(self):
+        """
+        Constructor.
+
+        """
         self.hand_type = HandType(0)
         self.card_list = []
 
     def __repr__(self):
+        """
+        String representation of the hand object.
+
+        :return: String name of the hand.
+        """
         return str(self.hand_type.name).replace("_", " ").capitalize()
 
+    def get_hand_type(self) -> HandType:
+        """
+        Returns the hand type enumeration that represents the hand.
+
+        :return: HandType enumeration.
+        """
+        # should evaluate_hand be called here?
+        return self.hand_type
+
     def evaluate_hand(self):
+        """
+        Determines the strength of the hand to update the hand_type enumeration.
+
+        :return: None.
+        """
         pass
 
     def append_card_list(self, card_list: list[Card]):
+        """
+        Add a specified list of cards to the list of cards.
+
+        :param card_list: List of card objects to be added.
+        :return: None.
+        """
         self.card_list += card_list
 
     def add_card(self, card: Card):
-        pass
+        """
+        Add a specified card ot the list of cards.
+
+        :param card: Card object to be added
+        :return: None.
+        """
+        self.card_list.append(card)
 
     def clear_card_list(self):
+        """
+        Removes all cards from the card list
+
+        :return: None.
+        """
         self.card_list = []
+
+    def _filter_hidden_cards(self):
+        """
+        Removes all hidden cards from the card list.
+
+        :return: None
+        """
+        pass
+
+    def _sort_cards(self):
+        """
+        Sorts the card list from highest value to lowest value.
+        :return:
+        """
